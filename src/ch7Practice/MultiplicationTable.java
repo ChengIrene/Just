1af80e8bb0d80ac8602e04c999f2449815a2f1fb
unknown <pmlhtoosvsyekm@gmail.com>
2017-11-02 23:54:42 +0800

    ch7Practice Multiplication Table

---
+++ src/ch7Practice/MultiplicationTable.java
@@ -0,0 +1,18 @@
+package ch7Practice;
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+
+       for ( int i=1; i <= 9; i++ ){
+		    	 
+		 for( int j=1; j <= 9; j++ ){
+		    		 
+		  System.out.print( j + "*" + i + " =" + (i*j) + "\t\t");
+    		
+		 }	
+		 
+		 System.out.println(" ");
+	}
+}
+}
